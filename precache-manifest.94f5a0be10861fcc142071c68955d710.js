5e77c9dab37d0f58a849325fe9910806e5f72454
Bowen Zhang <bowenzhang@gmail.com>
2020-05-26 22:40:00 -0400

    Updates

---
+++ precache-manifest.94f5a0be10861fcc142071c68955d710.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "066923e9d61b8417644fc5e6b293e64e",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "2bae56bd6f9e9fc13d3e",
+    "url": "/corona-tracker/static/css/main.1ea7c501.chunk.css"
+  },
+  {
+    "revision": "88fa3745b6d38eb13d63",
+    "url": "/corona-tracker/static/js/2.bda9ffec.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-tracker/static/js/2.bda9ffec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bae56bd6f9e9fc13d3e",
+    "url": "/corona-tracker/static/js/main.a33116d2.chunk.js"
+  },
+  {
+    "revision": "7e7c08e4a6218267afe9",
+    "url": "/corona-tracker/static/js/runtime-main.1b83197d.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/corona-tracker/static/media/image.d7265326.png"
+  }
+]);
